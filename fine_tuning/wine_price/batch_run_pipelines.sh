b5020a068dd42504deed3576bb4267f18129ed92
ChrisDEV <dev.xinyue.zhao@gmail.com>
2024-07-27 14:00:26 +0000

    move to wine_price dir

---
+++ fine_tuning/wine_price/batch_run_pipelines.sh
@@ -0,0 +1,5 @@
+#run 
+python pipeline.py --project_id "isochrone-isodistance" --predefined_acl "projectPrivate" --model_name "gemini-chat"
+python pipeline.py --project_id "isochrone-isodistance" --predefined_acl "projectPrivate" --model_name "gemma"
+python pipeline.py --project_id "isochrone-isodistance" --predefined_acl "projectPrivate" --model_name "chat-bison"  
+python pipeline.py --project_id "isochrone-isodistance" --predefined_acl "projectPrivate" --model_name "text-bison"  
